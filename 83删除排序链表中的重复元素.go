cd73ead23ae288e5ecc5d36ee6bbb62fc2cd56fe
konekos <newbiesad@gmail.com>
2021-03-26 11:02:25 +0800

    fix

---
+++ 83删除排序链表中的重复元素.go
@@ -0,0 +1,64 @@
+package main
+
+//存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+//
+// 返回同样按升序排列的结果链表。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,1,2]
+//输出：[1,2]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = [1,1,2,3,3]
+//输出：[1,2,3]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 链表中节点数目在范围 [0, 300] 内
+// -100 <= Node.val <= 100
+// 题目数据保证链表已经按升序排列
+//
+// Related Topics 链表
+// 👍 516 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	dummy := &ListNode{Val: -101}
+	dummy.Next = head
+	prev := dummy
+	curr := head
+
+	for curr != nil {
+		next := curr.Next
+		if curr.Val != prev.Val {
+			prev.Next = curr
+			prev = curr
+			curr = next
+		} else {
+			prev.Next = nil
+			curr.Next = nil
+			curr = next
+		}
+	}
+	return dummy.Next
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
